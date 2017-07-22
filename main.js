1a050ffe200575de2f3104c252295e15921d8ff3
Ronda Wylie <rondawylie@yahoo.com>
2017-07-21 19:46:04 -0500

    added links and projectsto portfolio

--- main.js
+++ main.js
@@ -51,3 +51,11 @@ $(document).ready(function(){
       }, 900, "easeOutBounce");
   });
 });
+
+$(".popup").click(function () {
+    var $this = $(this);
+    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 400, "height": 300});
+    var $title = $("<h1>").text($this.data("title"));
+    $("#video-view").html($title).append($iframe);
+    $iframe.wrap("<div class='class-video'>");
+});
